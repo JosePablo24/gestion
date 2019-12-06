4defdbf8ebcdfe12b73f6755fce17884f3656263
JosePablo24 <173198@ids.upchiapas.edu.mx>
2019-12-06 09:51:09 -0600

    ya esta quedando

--- src/components/servicios/axios.js
+++ src/components/servicios/axios.js
@@ -2,5 +2,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-    baseURL:`http://127.0.0.1:8000/api/v1/`    
+    baseURL:`http://controlbackend.ddns.net/api/v1/`
+    //local
+    //baseURL:`http://127.0.0.1:8000/api/v1/`
 });
